e5c12f0eff6a03a06db8a5878e144c5dbb756c5e
Idevaldo <inetomec@yahoo.com.br>
2022-05-04 10:49:27 -0300

    feat: :sparkles: Initial Setup

---
+++ src/test/java/com/idevbn/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idevbn.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
